6b7037481555835a57ec477cfc39525efe3bfd7e
redgoose <scripter@me.com>
2017-07-17 13:27:15 +0900

    안쓰지만 백업용 파일

--- src/res/StatusBar.js
+++ src/res/StatusBar.js
@@ -7,10 +7,9 @@ import css from './css';
 export default class StatusBar extends React.Component {
 
 	static defaultProps = {
-		height: 34,
-		delay: 3000,
-		color: null,
-		message: 'message',
+		height: 32,
+		delay: 2000,
+		speed: 200,
 	};
 
 	constructor(props) {
@@ -19,37 +18,57 @@ export default class StatusBar extends React.Component {
 		this.timer = null;
 		this.state = {
 			anim: new Animated.Value(0),
+			color: null,
+			message: ''
 		};
 	}
 
-	open(color, message) {
+	shouldComponentUpdate(nextProps, nextState) {
+		const { state } = this;
+
+		//if (state.stamp !== nextState.stamp) return true;
+		if (state !== nextState) return true;
+
+		return false;
+	}
+
+	open(color, message, delay) {
 		const { props, state } = this;
+
+		console.warn('OPEN STATUS BAR');
 
 		// clear timer
 		if (this.timer) {
+			console.log('CLEAR TIMEOUT')
 			clearTimeout(this.timer);
 			this.timer = null;
 		}
 
-		// play animate
-		Animated.timing(state.anim, {
-			easing: Easing.out(Easing.ease),
-			duration: 200,
-			toValue: 1
-		}).start(() => {
-			this.timer = setTimeout(() => this.close(), props.delay);
+		this.setState({ color, message }, () => {
+			// play animate
+			Animated.timing(state.anim, {
+				easing: Easing.out(Easing.ease),
+				duration: props.speed,
+				toValue: 1
+			}).start(() => {
+				this.timer = setTimeout(() => this.close(), delay || props.delay);
+			});
 		});
 	}
 
 	close() {
 		const { props, state } = this;
 
+		console.warn('CLOSE()');
+
 		// play animate
 		Animated.timing(state.anim, {
 			easing: Easing.out(Easing.ease),
-			duration: 200,
+			duration: props.speed,
 			toValue: 0
 		}).start();
+
+		this.setState({ stamp: state.stamp + 1 });
 	}
 
 	render() {
@@ -59,6 +78,8 @@ export default class StatusBar extends React.Component {
 			outputRange: [0 - props.height, 0]
 		});
 
+		console.warn('STATUS BAR RENDER');
+
 		return (
 			<Animated.View style={[
 				css.statusBar,
@@ -67,8 +88,15 @@ export default class StatusBar extends React.Component {
 					top: top,
 				}
 			]}>
-				<View style={[ css.bar, css[`bar_${props.color}`] ]}>
-					<Text numberOfLines={1} style={css.bar__text}>{props.message}</Text>
+				<View style={[ css.statusBar__bar, css[`statusBar__bar_${state.color}`] ]}>
+					<Text
+						numberOfLines={1}
+						style={[
+							css.statusBar__barText,
+							css[`statusBar__barText_${state.color}`],
+						]}>
+						{state.message}
+					</Text>
 				</View>
 			</Animated.View>
 		);
